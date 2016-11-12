a9de3587be8e3024307cfc766b6bbc000416c92d
Akash Trehan <akash.trehan123@gmail.com>
2016-11-13 02:40:30 +0530

    Initial commit

---
+++ analyzer.py
@@ -0,0 +1,15 @@
+import nltk
+
+class Analyzer():
+    """Implements sentiment analysis."""
+
+    def __init__(self, positives="positive-words.txt", negatives="negative-words.txt"):
+        """Initialize Analyzer."""
+
+        # TODO
+
+    def analyze(self, text):
+        """Analyze text for sentiment, returning its score."""
+
+        # TODO
+        return 0

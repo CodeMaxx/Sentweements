44ddc4efff9d1aeb5f57d9ecbaad348f8282643a
Akash Trehan <akash.trehan123@gmail.com>
2016-11-14 06:51:06 +0530

    Analyzer done

--- analyzer.py
+++ analyzer.py
@@ -3,13 +3,35 @@ import nltk
 class Analyzer():
     """Implements sentiment analysis."""
 
+
+    positive_words = [] # Stores positive words
+    negative_words = [] # Store negative words
+
     def __init__(self, positives="positive-words.txt", negatives="negative-words.txt"):
         """Initialize Analyzer."""
+        # Initialises class variables
+        for line in open(positives):
+            li = line.strip()
+            if not li.startswith(";"):
+                Analyzer.positive_words.append(li)
 
-        # TODO
+        for line in open(negatives):
+            li = line.strip()
+            if not li.startswith(";"):
+                Analyzer.negative_words.append(li)
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
 
-        # TODO
-        return 0
+        # Positive score if text is positive, negative if negative else zero
+
+        words = text.split(" ")
+        score = 0
+
+        for word in words:
+            if word.lower() in Analyzer.positive_words:
+                score = score + 1
+            elif word.lower() in Analyzer.negative_words:
+                score = score - 1
+
+        return score
